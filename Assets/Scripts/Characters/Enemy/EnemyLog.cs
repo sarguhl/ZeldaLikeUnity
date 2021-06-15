930cd3f0830131581734c18158e50466d573845e
Sarguhl <52204356+sarguhl1337@users.noreply.github.com>
2021-06-15 14:14:22 +0200

    15.06.21

--- Assets/Scripts/Characters/Enemy/EnemyLog.cs
+++ Assets/Scripts/Characters/Enemy/EnemyLog.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class EnemyLog : Enemy
 {
+    private Rigidbody2D myRigidbody;
+
     public Transform target;
     public Transform homePosition;
     public float chaseRadius;
     public float attackRadius;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyState.idle;
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         CheckDistance();
     }
@@ -25,7 +31,21 @@ public class EnemyLog : Enemy
     {
         if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            if (currentState == EnemyState.idle || currentState == EnemyState.walk && currentState != EnemyState.stagger)
+            {
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+            }
         }
     }
+
+    private void ChangeState(EnemyState newState)
+    {
+        if(currentState != newState)
+        {
+            currentState = newState;
+        }
+    }
+
 }
